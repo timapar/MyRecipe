5572d8b18f7ae2746c7a3d84409482bd637e1bdc
timapar <timothy_parrish@hotmail.com>
2021-07-07 12:56:03 -0400

    add third screen - My Recipe

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import RecipeView from './components/recipeView';
 import AddRecipe from './components/addRecipe';
+import Recipe from './components/recipe';
 
 const Stack = createStackNavigator();
 
@@ -12,6 +13,7 @@ const MyStack = () => {
     <Stack.Navigator>
       <Stack.Screen name="Recipes" component={RecipeView} />
       <Stack.Screen name="Add Recipe" component={AddRecipe} />
+      <Stack.Screen name="My Recipe" component={Recipe} />
     </Stack.Navigator>
   );
 };
