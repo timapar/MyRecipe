cd35a70ecf0173dd6fd772d10ddf4bab0cba00ec
timapar <timothy_parrish@hotmail.com>
2021-07-07 11:54:23 -0400

    implement basic structure

---
+++ components/recipe.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { View } from 'react-native';
+import styles from '../styles/';
+
+const Recipe = ({ recipe }) => {
+  return <View style={styles.tile}></View>;
+};
+
+export default Recipe;
