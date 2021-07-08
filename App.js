6bc3769512353a32034525e1cc316ea9d2e58f74
timapar <timothy_parrish@hotmail.com>
2021-07-08 08:42:40 -0400

    rename recipeView to recipeList

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import RecipeView from './components/recipeView';
+import RecipeList from './components/recipeList';
 import AddRecipe from './components/addRecipe';
 import Recipe from './components/recipe';
 
@@ -11,7 +11,7 @@ const Stack = createStackNavigator();
 const MyStack = () => {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Recipes" component={RecipeView} />
+      <Stack.Screen name="Recipes" component={RecipeList} />
       <Stack.Screen name="Add Recipe" component={AddRecipe} />
       <Stack.Screen name="My Recipe" component={Recipe} />
     </Stack.Navigator>
