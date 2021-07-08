31bbe2ece1bdfe3db2018d85144c0f452348e734
timapar <timothy_parrish@hotmail.com>
2021-07-07 20:49:43 -0400

    implement GET all-recipes and GET recipe endpoints

--- server/index.js
+++ server/index.js
@@ -1,9 +1,39 @@
 const express = require('express');
+const { addNewRecipe, getListOfAllRecipes, getRecipeDetails } = require('../database/models.js');
 const app = express();
 const port = process.env.PORT || 1337;
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+app.get('/all-recipes/:uid', (req, res) => {
+  getListOfAllRecipes(req.params.uid)
+    .then((results) => res.status(200).json(results.rows))
+    .catch((err) => {
+      console.error(err.stack);
+      res.sendStatus(500);
+    });
+});
+
+app.get('/recipe/:id', (req, res) => {
+  getRecipeDetails(req.params.id)
+    .then((results) => res.status(200).json(results.rows))
+    .catch((err) => {
+      console.error(err.stack);
+      res.sendStatus(500);
+    });
+});
+
+app.post('/recipe', (req, res) => {
+  addNewRecipe(req.body.data)
+    .then(() => res.sendStatus(201))
+    .catch((err) => {
+      console.error(err.stack);
+      res.sendStatus(500);
+    });
 });
 
 app.listen(port, () => {
