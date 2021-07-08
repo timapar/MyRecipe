c5def60fa2a65bbadd486e72035c96fe91a8edf7
timapar <timothy_parrish@hotmail.com>
2021-07-08 15:57:09 -0400

    implement resipe display

--- components/recipe.jsx
+++ components/recipe.jsx
@@ -1,9 +1,64 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, ScrollView } from 'react-native';
+import axios from 'axios';
 import styles from '../styles/';
 
-const Recipe = ({ recipe }) => {
-  return <View style={styles.tile}></View>;
-};
+class Recipe extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: props.route.params.recipeID,
+      user: null,
+      title: null,
+      ingredients: null,
+      instructions: null,
+      photo: null,
+    };
+  }
+
+  getDetailsOfRecipe(id) {
+    return axios.get(`http://192.168.1.7:1337/recipe/${id}`);
+  }
+
+  componentDidMount() {
+    this.getDetailsOfRecipe(this.state.id)
+      .then((results) => {
+        const recipe = results.data;
+        this.setState({
+          user: recipe.users_id,
+          title: recipe.title,
+          ingredients: recipe.ingredients,
+          instructions: recipe.instructions,
+          photo: recipe.photo,
+        });
+      })
+      .catch((err) => {
+        console.log('error with request');
+        console.error(err.stack);
+      });
+  }
+
+  render() {
+    if (this.state.title) {
+      return (
+        <ScrollView>
+          <View style={styles.colContainer}>
+            <Text style={styles.recipeTitle}>{this.state.title}</Text>
+            <Text style={styles.recipeSubtitle}>Ingredients:</Text>
+            <Text style={styles.recipeIngredients}>{this.state.ingredients}</Text>
+            <Text style={styles.recipeSubtitle}>Instructions:</Text>
+            <Text style={styles.recipeInstructions}>{this.state.instructions}</Text>
+          </View>
+        </ScrollView>
+      );
+    } else {
+      return (
+        <View style={styles.loading}>
+          <Text style={styles.loadingText}>Loading</Text>
+        </View>
+      );
+    }
+  }
+}
 
 export default Recipe;
