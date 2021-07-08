a3163c70a5a49019ec90d9fa0add900fb92a027c
timapar <timothy_parrish@hotmail.com>
2021-07-07 20:47:51 -0400

    implement getListOfAllRecipes and getRecipeDetails

---
+++ database/models.js
@@ -0,0 +1,32 @@
+const { pool } = require('./');
+
+module.exports.getListOfAllRecipes = (usersID) => {
+  const selectAllRecipesQryStr = `
+    SELECT id, title, photo
+    FROM recipes
+    WHERE users_id = ${usersID}
+    ORDER BY id DESC;
+  `;
+
+  return pool.query(selectAllRecipesQryStr);
+};
+
+module.exports.getRecipeDetails = (recipeID) => {
+  const selectRecipeQryStr = `
+    SELECT *
+    FROM recipes
+    WHERE id = ${recipeID};
+  `;
+
+  return pool.query(selectRecipeQryStr);
+};
+
+module.exports.addNewRecipe = ({ title, ingredients, instructions }) => {
+  const insertRecipeQryStr = `
+    INSERT INTO recipes(users_id, title, ingredients, instructions)
+    VALUES (1, '${title}', '${ingredients}', '${instructions}')
+    RETURNING id;
+  `;
+
+  return pool.query(insertRecipeQryStr);
+};
