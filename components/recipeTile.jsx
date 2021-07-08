2e4e1e5223314ffcc81797cff153a1dbd5619b8a
timapar <timothy_parrish@hotmail.com>
2021-07-08 11:21:27 -0400

    implement navigation to My Recipe on press

--- components/recipeTile.jsx
+++ components/recipeTile.jsx
@@ -2,14 +2,24 @@ import React from 'react';
 import { TouchableOpacity, ImageBackground, Text } from 'react-native';
 import styles from '../styles/';
 
-const RecipeTile = ({ recipe }) => {
+const RecipeTile = (props) => {
   return (
-    <TouchableOpacity style={styles.tile}>
+    <TouchableOpacity
+      style={styles.tile}
+      onPress={() => {
+        console.log(props.recipe.photo);
+        props.navigation.navigate('My Recipe', { recipeID: props.recipe.id });
+      }}
+    >
       <ImageBackground
         style={styles.tile}
-        source={require('/home/tim/HackReactor/MVP/MyRecipe/assets/default-recipe-big.png')}
+        source={
+          props.recipe.photo
+            ? { uri: props.recipe.photo }
+            : require('/home/tim/HackReactor/MVP/MyRecipe/assets/default-recipe-big.png')
+        }
       >
-        <Text style={styles.tileText}>{recipe.title}</Text>
+        <Text style={styles.tileText}>{props.recipe.title}</Text>
       </ImageBackground>
     </TouchableOpacity>
   );
